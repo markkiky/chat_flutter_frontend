c4ecf38d15ee3c46d9d219f3fdcee52eaa0ec197
Mark Ndungu <markkaris438@gmail.com>
2022-01-17 15:05:09 +0300

    chat app ui with welcome, sigin and chats page

---
+++ lib/screens/chats/body.dart
@@ -0,0 +1,49 @@
+import 'package:chat/components/filled_outline_button.dart';
+import 'package:chat/constants.dart';
+import 'package:chat/screens/messages/messages_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:chat/models/Chat.dart';
+import 'chat_card.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: kPrimaryColor,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(
+                kDefaultPadding, 0, kDefaultPadding, kDefaultPadding),
+            child: Row(
+              children: [
+                FillOutlineButton(press: () {}, text: "Recent Message"),
+                SizedBox(
+                  width: kDefaultPadding,
+                ),
+                FillOutlineButton(
+                  press: () {},
+                  text: "Active",
+                  isFilled: false,
+                )
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+            child: ListView.builder(
+          itemCount: chatsData.length,
+          itemBuilder: (context, index) => ChatCard(
+            chat: chatsData[index],
+            press: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MessagesScreen())),
+          ),
+        ))
+      ],
+    );
+  }
+}
---
+++ lib/screens/chats/chats_screen.dart
@@ -0,0 +1,64 @@
+import 'package:chat/screens/chats/body.dart';
+import 'package:chat/screens/messages/messages_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:chat/constants.dart';
+
+class ChatsScreen extends StatefulWidget {
+  const ChatsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: const Body(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const MessagesScreen())),
+        backgroundColor: kPrimaryColor,
+        child: const Icon(
+          Icons.person_add_alt_1,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.shifting,
+      currentIndex: _selectedIndex,
+      onTap: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.messenger), label: "Chats"),
+        BottomNavigationBarItem(icon: Icon(Icons.people), label: "People"),
+        BottomNavigationBarItem(icon: Icon(Icons.portrait), label: "Calls"),
+        BottomNavigationBarItem(
+            label: "profile",
+            icon: CircleAvatar(
+              radius: 14,
+              backgroundImage: AssetImage("assets/images/user_2.png"),
+            ))
+      ],
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: const Text("Chats"),
+      backgroundColor: kPrimaryColor,
+      actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+    );
+  }
+}
